384a1b9e55b19c325b8aa6a1e09307c5bf7a25ff
ascetik <ascetik@free.fr>
2024-01-09 08:38:29 +0100

    Option interface doc

--- src/Types/Option.php
+++ src/Types/Option.php
@@ -1,10 +1,24 @@
 <?php
 
+/**
+ * This is part of the ascetik/mono package
+ *
+ * @package    Mono\Core
+ * @category   Interface
+ * @license    https://opensource.org/license/mit/  MIT License
+ * @copyright  Copyright (c) 2023, Vidda
+ * @author     Vidda <vidda@ascetik.fr>
+ */
+
 declare(strict_types=1);
 
 namespace Ascetik\Mono\Types;
 
-
+/**
+ * Describe the behavior of a Maybe Option
+ *
+ * @version 1.0.0
+ */
 interface Option
 {
     public function apply(callable $function): mixed;
