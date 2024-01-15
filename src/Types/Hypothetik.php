b210a22c02deafedcdade5c789a1a0feb04b27c8
ascetik <ascetik@free.fr>
2024-01-15 17:15:32 +0100

    nouvelle interface Hypothetik

---
+++ src/Types/Hypothetik.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ascetik\Hypothetik\Types;
+
+interface Hypothetik
+{
+    // public function apply(callable $function, mixed ...$arguments): mixed;
+
+}
