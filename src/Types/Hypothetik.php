a8d4f23abc6065db800c8018628e0fb8586e281c
ascetik <ascetik@free.fr>
2024-01-15 18:30:53 +0100

    j'avais oublié les paramètres dans either...

--- src/Types/Hypothetik.php
+++ src/Types/Hypothetik.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace Ascetik\Hypothetik\Types;
 
 use Ascetik\Hypothetik\Core\Maybe;
+use Ascetik\Hypothetik\Core\Either;
 
 interface Hypothetik
 {
     public function apply(callable $function, mixed ...$arguments): mixed;
+    public function either(callable $function, mixed ...$arguments): Either;
     public function isValid(): bool;
     public function otherwise(mixed $value): self;
     public function then(callable $function, mixed ...$arguments): self;
