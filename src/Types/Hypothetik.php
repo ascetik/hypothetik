4e77c44ec246847a0411ab1cde79099ed5bacba0
ascetik <ascetik@free.fr>
2024-01-15 18:35:42 +0100

    Hypothetik doc

--- src/Types/Hypothetik.php
+++ src/Types/Hypothetik.php
@@ -1,12 +1,27 @@
 <?php
 
+/**
+ * This is part of the ascetik/mono package
+ *
+ * @package    Mono\Core
+ * @category   Interface
+ * @license    https://opensource.org/license/mit/  MIT License
+ * @copyright  Copyright (c) 2023, Vidda
+ * @author     Vidda <vidda@ascetik.fr>
+ */
+
 declare(strict_types=1);
 
 namespace Ascetik\Hypothetik\Types;
 
-use Ascetik\Hypothetik\Core\Maybe;
 use Ascetik\Hypothetik\Core\Either;
 
+/**
+ * Describe the behavior of a class able
+ * to handle the validity of a value
+ *
+ * @version 1.0.0
+ */
 interface Hypothetik
 {
     public function apply(callable $function, mixed ...$arguments): mixed;
